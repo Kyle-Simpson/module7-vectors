d2ee6ad108f4a2b1dd7ca6732762a37df8355e33
Kyle Simpson <kes97@uw.edu>
2017-01-17 13:40:41 -0800

    Complete exercise

---
+++ exercise-5.R
@@ -0,0 +1,27 @@
+# Exercise 5: Vector practice
+
+# Create a vector `words` of 6 (or more) words.
+# You can Google for a "random word generator" if you wish!
+words <- c("this", "that", "those", "these", "them", "they")
+
+# Create a new vector `words.of.the.day` that is your `words` vector with the string
+# "is the word of the day!" pasted on to the end.
+# BONUS: Surround the word in quotes (single or double) (e.g., `"data" is the word of the day!`)
+words.of.the.day <- paste(words, "is the word of the day")
+
+# Create a vector `a.f.words` which are the elements in `words` that start with "a" through "f"
+# Hint: use a comparison operator to see if the word comes before "f" alphabetically!
+# Tip: make sure all the words are lower-case, and only consider the first letter of the word!
+a.f.words <- words[a:f]
+
+# Create a vector `g.m.words` which are the elements in `words` that start with "g" through "m"
+g.m.words <- words[g:m]
+
+# Define a function `WordBin` that takes in three arguments: a vector of words, and two letters. The function
+# should return a vector of words that go between those letters alphabetically.
+WordBin <- function(words, letter.1, letter.2) {
+  new.words <- c(words[letter.1:letter.2])
+}
+
+# Use your `WordBin` function to determine which of your words start with "e" through "q"
+WordBin(words, a.f.words, g.m.words)
