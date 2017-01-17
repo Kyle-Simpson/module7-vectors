b79c35005cef5ff5c1c3ccacb3cab0cb9365f6ca
Kyle Simpson <kes97@uw.edu>
2017-01-17 13:47:16 -0800

    Fix bug in comparison

--- exercise-5.R
+++ exercise-5.R
@@ -12,16 +12,16 @@ words.of.the.day <- paste(words, "is the word of the day")
 # Create a vector `a.f.words` which are the elements in `words` that start with "a" through "f"
 # Hint: use a comparison operator to see if the word comes before "f" alphabetically!
 # Tip: make sure all the words are lower-case, and only consider the first letter of the word!
-a.f.words <- words[a:f]
+a.f.words <- words[words <= 'f']
 
 # Create a vector `g.m.words` which are the elements in `words` that start with "g" through "m"
-g.m.words <- words[g:m]
+g.m.words <- words[words >= 'g' && words <= 'm']
 
 # Define a function `WordBin` that takes in three arguments: a vector of words, and two letters. The function
 # should return a vector of words that go between those letters alphabetically.
 WordBin <- function(words, letter.1, letter.2) {
-  new.words <- c(words[letter.1:letter.2])
+  new.words <- c(words[words >= letter.1 && words <= letter.2])
 }
 
 # Use your `WordBin` function to determine which of your words start with "e" through "q"
-WordBin(words, a.f.words, g.m.words)
+print(WordBin(words, 'e', 'q'))
